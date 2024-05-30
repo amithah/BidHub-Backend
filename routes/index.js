6dcbbf99ff4be2fd581a0318593b3727bee607d0
amithah <amithah.nithin@gmail.com>
2024-05-30 12:25:17 +0400

    test

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { upload } = require("../controllers/uploadController");
 const router = express.Router();
-
+router.get('/',
+    (req, res) => {
+        res.send("Hello World");
+    });
 router.use(require("./auth"));
 router.use(require("./users"));
 router.use(require("./item"));
